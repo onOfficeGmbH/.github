69db6951465534e9ac1074e6df842a952ea39d4a
Dominik Kaisers <d.kaisers@onoffice.de>
2022-03-28 12:26:11 +0000

    Add action to create a task via the onOffice API

---
+++ actions/create-task/entrypoint.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use onOffice\SDK\onOfficeSDK;
+
+$optionsConfig = [
+    'token:',               // string (required)
+    'secret:',              // string (required)
+    'api-version::',        // string [stable|latest] (default: latest)
+    'subject:',             // string (required)
+    'text:',                // string (required)
+    'type:',                // int (required)
+    'responsible:',         // string (required)
+    'responsibleIsGroup::', // int [0|1] (default: 1)
+    'editor::',             // string
+    'project::',            // int
+];
+$options = getopt('', $optionsConfig);
+
+$sdk = new onOfficeSDK();
+$sdk->setApiVersion($options['api-version'] ?? 'latest');
+
+$parameters = [
+    'data' => [
+        'Betreff' => $options['subject'],
+        'Aufgabe' => $options['text'],
+        'Verantwortung' => $options['responsible'],
+        'Art' => $options['type'],
+    ],
+    'responsibilityByGroup' => ((int) ($options['responsibleIsGroup'] ?? 1)),
+];
+
+if (isset($options['editor'])) {
+    $parameters['data']['Bearbeiter'] = $options['editor'];
+}
+
+if (isset($options['project'])) {
+    $parameters['relatedProjectIds'] = [ $options['project'] ];
+}
+
+$handleCreateTask = $sdk->callGeneric(onOfficeSDK::ACTION_ID_CREATE, 'task', $parameters);
+$sdk->sendRequests($options['token'], $options['secret']);
